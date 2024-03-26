bd41dd2a6784d035b48556c8c2fc00fa069d0471
perompak_kode <iqbaltech29@gmail.com>
2024-03-27 02:27:45 +0700

    add package helper, ui, insert, main. add file NoteAddUpdateActivity

---
+++ app/src/main/java/com/jimbonlemu/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.jimbonlemu.mynoteapps.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jimbonlemu.mynoteapps.R
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_add_update)
+    }
+}
--- app/src/main/java/com/jimbonlemu/mynoteapps/ui/main/MainActivity.kt
+++ app/src/main/java/com/jimbonlemu/mynoteapps/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jimbonlemu.mynoteapps
+package com.jimbonlemu.mynoteapps.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jimbonlemu.mynoteapps.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
